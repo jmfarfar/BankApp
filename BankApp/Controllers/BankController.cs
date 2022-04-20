a488e9a0b8baca46e00e94ee6d8afa7afef3903a
Jose Ramos <jomaramos4@gmail.com>
2022-04-20 01:37:54 -0700

    Login, Create and Update APIs implemented

---
+++ BankApp/Controllers/BankController.cs
@@ -0,0 +1,57 @@
+﻿using BankApp.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using System.Linq;
+using BankApp.Repository;
+
+namespace BankApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankController : ControllerBase
+    {
+        private readonly BankDbContext _db;
+        private readonly IBankRepository _bankRepository;
+
+        public BankController(BankDbContext db, IBankRepository bankRepository)
+        {
+            _db = db;
+            _bankRepository = bankRepository;
+        }
+
+
+        // Login API 
+        [HttpGet]
+        public ActionResult<User> Login(string login, string password)
+        {
+            var user = _bankRepository.Login(login, password);
+            if (user is not null)
+            {
+                return Ok(user);
+            }
+            return BadRequest();
+            
+        }
+
+
+        // Create User API (for internal purposes to populate the database)
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(User user)
+        {
+            _db.Add(user);
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+        // Update Balance API        
+        [HttpPut]
+        public async Task<ActionResult> UpdateBalance(string login, string password, decimal newBalance)
+        {
+            _bankRepository.Update(login,password,newBalance);
+            return Ok();            
+        }
+
+        // Delete User API
+    }
+}
---
+++ BankApp/Repository/IBankRepository.cs
@@ -0,0 +1,11 @@
+﻿using BankApp.Entities;
+
+namespace BankApp.Repository
+{
+    public interface IBankRepository
+    {
+        public User Login(string login, string password);
+        public void Update(string login, string password, decimal newBalance);
+        public void Delete(string login, string password, string userToDelete);
+    }
+}
